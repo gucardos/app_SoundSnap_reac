b1f8338db149d8e1c4edbd73fe906505ceb7a776
Gustavo Santana Cardoso <134300180+gucardos@users.noreply.github.com>
2024-11-18 10:38:59 +0000

    arrumei o estilo para o headear e corrigi os erros com o expo go

--- Soundsnap/App.tsx
+++ Soundsnap/App.tsx
@@ -1,12 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TextInput, View } from 'react-native';
+import logo from './assets/logo_soundsnap_claro.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text> Hello world!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Image source={logo}
+          style={styles.ImagemLogo}/>
+        <TextInput style={styles.inputHeader} defaultValue='O quê você quer ouvir hoje?'/>
+      </View>
+      <Text style={styles.TituloDefault}>Hello World!</Text>
     </View>
   );
 }
@@ -14,8 +17,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#d4cdc5'
   },
+  TituloDefault:{
+    fontSize: 30,
+    color: '#191013'
+  },
+  ImagemLogo:{
+    width: 80,
+    height: 80,
+    paddingTop: 0,
+    paddingLeft: 0
+  },
+  header:{
+    backgroundColor: '#191013',
+    height:80,
+    width:'auto',
+    paddingTop: 0,
+    paddingLeft: 0,
+    flexDirection:'row',
+    alignItems:'center'
+  },
+  inputHeader:{
+    color:'#191013',
+    backgroundColor:'#d4cdc5',
+    borderRadius:20,
+    height:30,
+    width:250,
+    textAlign:'center'
+  }
 });
