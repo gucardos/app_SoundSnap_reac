ab513a7da337f66945057040551f71e2318b41ee
Gustavo Santana Cardoso <134300180+gucardos@users.noreply.github.com>
2024-11-19 00:29:29 +0000

    placeholder e corrigido o erro da importação da imagem

--- Soundsnap/App.tsx
+++ Soundsnap/App.tsx
@@ -1,14 +1,14 @@
 import { Image, StyleSheet, Text, TextInput, View } from 'react-native';
-import logo from './assets/logo_soundsnap_claro.png';
+
 import styles from './src/styles.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Image source={logo}
+        <Image source={require('./assets/logo_soundsnap_claro.png')}
           style={styles.ImagemLogo}/>
-        <TextInput style={styles.inputHeader} defaultValue='O quê você quer ouvir hoje?'/>
+        <TextInput style={styles.inputHeader} placeholder='O quê você quer ouvir hoje?'/>
       </View>
       <Text style={styles.TituloDefault}>Hello World!</Text>
     </View>
